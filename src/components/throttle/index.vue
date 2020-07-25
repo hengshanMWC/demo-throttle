b7203c3574ba30239bf7e70d2402fb47835ac6c6
mwc <1819293933@qq.com>
2020-07-25 11:20:28 +0800

    feat: use

--- src/components/throttle/index.vue
+++ src/components/throttle/index.vue
@@ -4,7 +4,8 @@
   </div>
 </template>
 <script>
-import { defineComponent, ref, onUnmounted, watch } from 'vue'
+import { defineComponent, onUnmounted, watch } from 'vue'
+import { useTime, useCache } from './use.js'
 export default defineComponent({
   name: 'throttle',
   props: {
@@ -14,9 +15,9 @@ export default defineComponent({
       default: 60
     },
     isCache: Boolean,
-    cacheObject: {
-      type: Object,
-      default: () => localStorage
+    cacheType: {
+      type: String,
+      default: 'localStorage'
     },
     cacheID: {
       validator: () => true,
@@ -24,43 +25,19 @@ export default defineComponent({
     }
   },
   // 数据出口
-  setup (props, content) {
-    // 响应式时间
-    const timeRef = ref(0)
-    // 定时器id
-    let timer
-    let loading = false
-    /** 
-     * 开始
-     * @param {Function} cd
-    */
-    async function start (cd) {
-      if (timeRef.value === 0 && !loading) {
-        loading = true
-        const b = await cd()
-        if (b) {
-          timeRef.value = props.seconds
-          content.emit('change', timeRef.value)
-          startTime()
-        }
-        loading = false
-      } else {
-        content.emit('ongoing', timeRef.value)
-      }
-    }
-    // 倒计时
-    function startTime () {
-      clearTimeout(timer)
-      timer = setTimeout(() => {
-        if (timeRef.value <= 0) {
-          clearTimeout(timer)
-        } else {
-          content.emit('change', --timeRef.value)
-          startTime()
-        }
-      }, 1000)
-    }
-    // 缓存模块
+  setup (props, { emit }) {
+    const {
+      timeRef,
+      timerRef,
+      start,
+      startTime
+    } = useTime(props.seconds, emit)
+    const { 
+      getCache,
+      setCache,
+      removeCache
+    } = useCache(props.cacheID, props.cacheType)
+    // 监听
     watch(() => timeRef.value, function (value) {
       if (props.isCache) {
         if (value <= 0) {
@@ -70,30 +47,18 @@ export default defineComponent({
         }
       }
     })
-    const key = 'throttle' + props.cacheID
-    function getCache () {
-      const time = Number(props.cacheObject.getItem(key)) - Date.now()
-      return time > 0 ? time / 1000 : 0
-    }
-    function setCache (seconds) {
-      const time = seconds * 1000 + Date.now()
-      props.cacheObject.setItem(key, time.toString())
-    }
-    function removeCache () {
-      props.cacheObject.removeItem(key)
-    }
-    // 初始化
-    (function () {
+    // 销毁记得清除定时器
+    onUnmounted(() => {
+      clearTimeout(timerRef.value)
+    })
+    function init () {
       if (props.isCache) {
         timeRef.value = Math.round(getCache())
         startTime()
       }
       removeCache()
-    })()
-    // 销毁记得清除定时器
-    onUnmounted(() => {
-      clearTimeout(timer)
-    })
+    }
+    init()
     return {
       start,
       timeRef
---
+++ src/components/throttle/use.js
@@ -0,0 +1,61 @@
+import { ref } from 'vue'
+export function useTime (seconds, emit) {
+  const timeRef = ref(0)
+  const timerRef = ref(0)
+  let loading = false
+  /** 
+   * 开始
+   * @param {Function} cd
+  */
+  async function start (cd) {
+    if (timeRef.value === 0 && !loading) {
+      loading = true
+      const b = await cd()
+      if (b) {
+        timeRef.value = seconds
+        emit('change', timeRef.value)
+        startTime()
+      }
+      loading = false
+    } else {
+      emit('ongoing', timeRef.value)
+    }
+  }
+  // 倒计时
+  function startTime () {
+    clearTimeout(timerRef.value)
+    timerRef.value = setTimeout(() => {
+      if (timeRef.value <= 0) {
+        clearTimeout(timerRef.value)
+      } else {
+        emit('change', --timeRef.value)
+        startTime()
+      }
+    }, 1000)
+  }
+  return {
+    timeRef,
+    timerRef,
+    start,
+    startTime
+  }
+}
+export function useCache (cacheID, cacheType) {
+  const key = 'throttle' + cacheID
+  function getCache () {
+    const time = Number(window[cacheType].getItem(key)) - Date.now()
+    return time > 0 ? time / 1000 : 0
+  }
+  function setCache (seconds) {
+    const time = seconds * 1000 + Date.now()
+    window[cacheType].setItem(key, time.toString())
+  }
+  function removeCache () {
+    window[cacheType].removeItem(key)
+  }
+  return {
+    getCache,
+    setCache,
+    removeCache
+  }
+}
